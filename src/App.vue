7b148ba06bbb22ebf1dc270b4132415bd40337be
eyitene solomon <solomoneyitene@gmail.com>
2020-07-21 23:05:39 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <p>Hello</p>
+    <h1 class="red">SUb 
+    </h1>
+      <p>d</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+
+  
+  }
+
+</style>
